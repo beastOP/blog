9661b069368df86bfd758703179a830382d1ef49
Omkar Patil <omkarpatil290@gmail.com>
2023-09-04 17:30:18 +0530

    update name

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 import blog, { ga } from "https://deno.land/x/blog@0.5.0/blog.tsx";
 
 blog({
-  title: "Omkar's Blog",
-  author: "Omkar Patil",
+  title: "X9 theta",
+  author: "X9 theta",
   description: "Passionate self-taught developer continuously improving.",
   avatar: "./op.jpeg",
   avatarClass: "rounded-full",
