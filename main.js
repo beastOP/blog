4a72b3cf147b54aa5eaaf748cd604cc1beb01f2e
Omkar Patil <omkarpatil290@gmail.com>
2024-06-01 22:32:11 +0530

    chore: update bio

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ import blog, { ga } from "https://deno.land/x/blog@0.5.0/blog.tsx";
 blog({
   title: "Omkar Patil",
   author: "Omkar Patil",
-  description: "Passionate self-taught developer continuously improving.",
+  description: "Relentless self-taught developer, always leveling up.",
   avatar: "./op.jpeg",
   avatarClass: "rounded-full",
   lang: "en",
