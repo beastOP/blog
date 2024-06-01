ebbf48251a3b86eec736973582ec71d826996cdb
Omkar Patil <omkarpatil290@gmail.com>
2024-06-01 22:29:38 +0530

    chore: update author

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 import blog, { ga } from "https://deno.land/x/blog@0.5.0/blog.tsx";
 
 blog({
-  title: "X9 theta",
-  author: "X9 theta",
+  title: "Omkar Patil",
+  author: "Omkar Patil",
   description: "Passionate self-taught developer continuously improving.",
   avatar: "./op.jpeg",
   avatarClass: "rounded-full",
